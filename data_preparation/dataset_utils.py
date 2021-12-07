8f686713faf5a801d6066d885345bfa5b7f36073
SamSamhuns <samhunsadamant@gmail.com>
2021-12-07 11:09:01 +0400

    Rename npz_tf conv script, reorg context mgers

--- data_preparation/dataset_utils.py
+++ data_preparation/dataset_utils.py
@@ -44,26 +44,25 @@ def _get_tfrecord_path(shard_id, tfrecord_dir_path):
 
 def _convert_dataset(fnames, tfrecord_dir_path, num_shards):
     num_per_shard = int(math.ceil(len(fnames) / float(num_shards)))
-    with tf.Graph().as_default():
-        with session.Session():
-            for shard_id in range(num_shards):
-                output_tfr_path = _get_tfrecord_path(
-                    shard_id, tfrecord_dir_path=tfrecord_dir_path)
-                options = tf.io.TFRecordOptions()  # GZIP NONE ZLIB
-                with tf.io.TFRecordWriter(output_tfr_path, options=options) as tfrecord_writer:
-                    start_ndx = shard_id * num_per_shard
-                    end_ndx = min((shard_id + 1) * num_per_shard, len(fnames))
-                    for i in range(start_ndx, end_ndx):
-                        sys.stdout.write(
-                            '\r>> Writing record %d/%d in shard %d' % (i + 1, len(fnames), shard_id + 1))
-                        sys.stdout.flush()
-                        data = np.load(fnames[i])
-                        img = data['image']
-                        class_id = data['class_id']
-                        height, width, c = img.shape
-                        img = img.tobytes()
-                        example = image_to_tfexample(
-                            img, class_id, height, width, c)
-                        tfrecord_writer.write(example.SerializeToString())
+    with tf.Graph().as_default(), session.Session():
+        for shard_id in range(num_shards):
+            output_tfr_path = _get_tfrecord_path(
+                shard_id, tfrecord_dir_path=tfrecord_dir_path)
+            options = tf.io.TFRecordOptions()
+            with tf.io.TFRecordWriter(output_tfr_path, options=options) as tfrecord_writer:
+                start_ndx = shard_id * num_per_shard
+                end_ndx = min((shard_id + 1) * num_per_shard, len(fnames))
+                for i in range(start_ndx, end_ndx):
+                    sys.stdout.write(
+                        '\r>> Writing record %d/%d in shard %d' % (i + 1, len(fnames), shard_id + 1))
+                    sys.stdout.flush()
+                    data = np.load(fnames[i])
+                    img = data['image']
+                    class_id = data['class_id']
+                    height, width, c = img.shape
+                    img = img.tobytes()
+                    example = image_to_tfexample(
+                        img, class_id, height, width, c)
+                    tfrecord_writer.write(example.SerializeToString())
     sys.stdout.write('\n')
     sys.stdout.flush()
