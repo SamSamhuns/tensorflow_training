b91ced05ef9622f7113d6f0e2801db93b6282b37
SamSamhuns <samhunsadamant@gmail.com>
2022-01-05 22:37:11 +0400

    Fix test metrics & add sklearn metrics

--- test.py
+++ test.py
@@ -4,7 +4,9 @@ load_dotenv(".env")
 
 import tqdm
 import argparse
+import numpy as np
 import tensorflow as tf
+import tf_train.metric as module_metric
 from tf_train.config_parser import ConfigParser
 
 
@@ -15,43 +17,59 @@ def get_class_name_list(mapping_file):
     2   class3
     """
     map_list = []
-    with open(mapping_file, 'r') as fmap:
+    with open(str(mapping_file), 'r') as fmap:
         for i, line in enumerate(fmap):
             line = line.strip().split('\t')
             map_list.append(line[1])
-    return map_list
+    return sorted(map_list)
 
 
 def test(config):
-    model = tf.keras.models.load_model(config.resum)
+    logger = config.get_logger('train')
+
+    model = tf.keras.models.load_model(config.resume)
     h, w, _ = config.model.args.input_shape
+
     test_ds = tf.keras.preprocessing.image_dataset_from_directory(
         config["data"]["test_data_dir"],
         labels='inferred',
-        class_names=get_class_name_list(config["tester"]),
+        class_names=get_class_name_list(
+            config["tester"]["class_map_txt_path"]),
         image_size=(h, w),
         seed=None,
         shuffle=False,
         batch_size=32)
 
     # print(test_ds.class_names)
-    total_classes = config["data"]["num_classes"]
-    top1_acc = tf.keras.metrics.TopKCategoricalAccuracy(k=1)
-    top2_acc = tf.keras.metrics.TopKCategoricalAccuracy(k=2)
-    top3_acc = tf.keras.metrics.TopKCategoricalAccuracy(k=3)
+    n_cls = config["data"]["num_classes"]
 
+    agg_pred, agg_label = [], []
     for image_batch, labels_batch in tqdm.tqdm(test_ds):
         image_batch = tf.cast(image_batch, tf.float32)
         image_batch = config.model.parent_module.preprocess_input(image_batch)
         output_batch = model(image_batch)
-        top1_acc.update_state(tf.one_hot(labels_batch, total_classes), output_batch)
-        top2_acc.update_state(tf.one_hot(labels_batch, total_classes), output_batch)
-        top3_acc.update_state(tf.one_hot(labels_batch, total_classes), output_batch)
-    print(f"Statistics for model: {config.resume}")
 
-    print(f"Top 1 accuracy: {top1_acc.result().numpy() * 100:.2f}%")
-    print(f"Top 2 accuracy: {top2_acc.result().numpy() * 100:.2f}%")
-    print(f"Top 3 accuracy: {top3_acc.result().numpy() * 100:.2f}%")
+        agg_pred.append(output_batch.numpy())
+        agg_label.append(labels_batch.numpy())
+    # shape=(len dataloader, bsize, n_cls)
+    agg_pred = np.concatenate(agg_pred, axis=0)
+    # shape=(len dataloader, bsize)
+    agg_label = np.concatenate(agg_label, axis=0)
+    # combine dataloader len & bsize axes
+    agg_pred = agg_pred.reshape(-1, agg_pred.shape[-1])
+    agg_label = agg_label.flatten()
+
+    logger.info(f"Statistics for model: {config.resume}")
+    met_val_dict = {}
+    met_func_dict = {_metric: config.init_ftn(["test_metrics", _metric], module_metric, num_classes=n_cls)
+                     if _metric in {"acc_per_class", "confusion_matrix"}
+                     else config.init_ftn(["test_metrics", _metric], module_metric)
+                     for _metric in config["test_metrics"]}
+    for met, met_func in met_func_dict.items():
+        met_val_dict[met] = met_func(agg_label, agg_pred)
+
+    log = {met: met_val for met, met_val in met_val_dict.items()}
+    logger.info(f"test: {(log)}")
 
 
 def main():
--- tf_train/metric/metrics.py
+++ tf_train/metric/metrics.py
@@ -1,5 +1,10 @@
+import numpy as np
 import tensorflow as tf
+from sklearn.metrics import top_k_accuracy_score, classification_report
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
+
+# ############################### tf based metrics #############################
 
 def CategoricalAccuracy(**kwargs):
     return tf.keras.metrics.CategoricalAccuracy(**kwargs)
@@ -7,3 +12,94 @@ def CategoricalAccuracy(**kwargs):
 
 def TopKCategoricalAccuracy(**kwargs):
     return tf.keras.metrics.TopKCategoricalAccuracy(**kwargs)
+
+
+# ################### test dataset only uses numpy metrics #####################
+# ############################## numpy based metrics ###########################
+
+
+def accuracy(target: np.array, output: np.array):
+    """
+    Accuracy: TP + TN / (TP + TN + FP + FN)
+    equivalent to:
+        pred = np.argmax(output, axis=1)
+        correct = np.sum(pred == target)
+        return correct / len(target)
+    """
+    pred = np.argmax(output, axis=1)
+    assert pred.shape[0] == len(target)
+    return accuracy_score(target, pred)
+
+
+def confusion_matrix(target: np.array, output: np.array, num_classes: int):
+    """
+    Accumulates the confusion matrix
+    """
+    conf_matrix = np.zeros([num_classes, num_classes])
+    pred = np.argmax(output, 1)
+    for t, p in zip(target.reshape(-1), pred.reshape(-1)):
+        conf_matrix[t, p] += 1
+    return conf_matrix
+
+
+def precision(target: np.array, output: np.array, average: str = "weighted"):
+    """
+    Precision: TP / (TP + FP)
+    """
+    valid_avgs = {"micro", "macro", "weighted"}
+    if average not in valid_avgs:
+        raise ValueError(f"{average} mode is not supported. Use from {valid_avgs}")
+    pred = np.argmax(output, axis=1)
+    assert pred.shape[0] == len(target)
+    return precision_score(target, pred, average=average)
+
+
+def recall(target: np.array, output: np.array, average: str = "weighted"):
+    """
+    Recall: TP / (TP + FN)
+    """
+    valid_avgs = {"micro", "macro", "weighted"}
+    if average not in valid_avgs:
+        raise ValueError(f"{average} mode is not supported. Use from {valid_avgs}")
+    pred = np.argmax(output, axis=1)
+    assert pred.shape[0] == len(target)
+    return recall_score(target, pred, average=average)
+
+
+def f1score(target: np.array, output: np.array, average: str = "weighted"):
+    """
+    F1 score: (2 * p * r) / (p + r)
+    """
+    valid_avgs = {"micro", "macro", "weighted"}
+    if average not in valid_avgs:
+        raise ValueError(f"{average} mode is not supported. Use from {valid_avgs}")
+    pred = np.argmax(output, axis=1)
+    assert pred.shape[0] == len(target)
+    return f1_score(target, pred, average=average)
+
+
+def accuracy_mse(target: np.array, output: np.array):
+    """
+    Accuracy when using regression rather than classification.
+    """
+    assert len(output) == len(target)
+    correct = np.sum(((output - target).abs() < 1))
+    return correct / len(target)
+
+
+def acc_per_class(target: np.array, output: np.array, num_classes: int):
+    """
+    Calculates acc per class
+    """
+    conf_mat = confusion_matrix(target, output, num_classes)
+    return conf_mat.diagonal() / conf_mat.sum(1)
+
+
+def top_k_acc(target: np.array, output: np.array, k: int):
+    return top_k_accuracy_score(y_true=target, y_score=output, k=k)
+
+
+def classification_report_sklearn(target: np.array, output: np.array, target_names: list = None):
+    pred = np.argmax(output, axis=1)
+    assert pred.shape[0] == len(target)
+    return classification_report(target, pred, target_names=target_names)
